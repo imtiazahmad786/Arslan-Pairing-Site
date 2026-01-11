fa35ecfa820c577952511d337a4001c0fa545c43
ArslanMD Official <numberkliye@gmail.com>
2026-01-11 12:11:23 +0500

    𝐀𝐑𝐒𝐋𝐀𝐍-𝐓𝐄𝐂𝐇

--- arslan.js
+++ arslan.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
     console.log(`
-Don't Forget To Give Star
+Don't Forget To Give Star Arslan-MD ⭐ 
 
  Server running on http://localhost:` + PORT)
 })
